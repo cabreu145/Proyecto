33253080046cb7dfa66e15d675d7efb280dc22c0
Carlos Abreu <35485515+cabreu145@users.noreply.github.com>
2020-07-15 15:32:37 -0500

    preparando Delete

--- formvalidation/lib/src/pages/home_page.dart
+++ formvalidation/lib/src/pages/home_page.dart
@@ -25,11 +25,33 @@ class HomePage extends StatelessWidget {
       builder:
           (BuildContext context, AsyncSnapshot<List<ProductoModel>> snapshot) {
         if (snapshot.hasData) {
-          return Container();
+          final productos = snapshot.data;
+          return ListView.builder(
+            itemCount: productos.length,
+            itemBuilder: (context, i) => _crearItem(
+                context, productos[i]), //Instancia del producto model
+          );
         } else {
           return Center(child: CircularProgressIndicator());
         }
       },
+    );
+  }
+
+  Widget _crearItem(BuildContext context, ProductoModel producto) {
+    return Dismissible(
+      key: UniqueKey(),
+      background: Container(
+        color: Colors.red,
+      ),
+      onDismissed: (direction) {
+        // TODO: Borrar producto
+      },
+      child: ListTile(
+        title: Text('${producto.titulo} - ${producto.valor}'),
+        subtitle: Text(producto.id),
+        onTap: () => Navigator.pushNamed(context, 'producto'),
+      ),
     );
   }
 
