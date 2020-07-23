331230327f4f46b8125631aca742c59873ed79cb
Carlos Abreu <35485515+cabreu145@users.noreply.github.com>
2020-07-23 00:00:03 -0500

    productos bloc

--- formvalidation/lib/src/bloc/provider.dart
+++ formvalidation/lib/src/bloc/provider.dart
@@ -1,9 +1,13 @@
 //injerithed widger modificado
 import 'package:flutter/material.dart';
 import 'package:formvalidation/src/bloc/login_bloc.dart';
+import 'package:formvalidation/src/bloc/productos_bloc.dart';
 export 'package:formvalidation/src/bloc/login_bloc.dart';
 
 class Provider extends InheritedWidget {
+  final loginBloc = new LoginBloc();
+  final _productosbloc = new ProductosBloc();
+
   static Provider _instancia;
 
   factory Provider({Key key, Widget child}) {
@@ -15,8 +19,6 @@ class Provider extends InheritedWidget {
   }
   Provider._internal({Key key, Widget child}) : super(key: key, child: child);
 
-  final loginBloc = LoginBloc();
-
   // Provider({Key key, Widget child}) : super(key: key, child: child);
 
   @override
@@ -24,5 +26,11 @@ class Provider extends InheritedWidget {
 
   static LoginBloc of(BuildContext context) {
     return context.dependOnInheritedWidgetOfExactType<Provider>().loginBloc;
+  }
+
+  static ProductosBloc productosBloc(BuildContext context) {
+    return context
+        .dependOnInheritedWidgetOfExactType<Provider>()
+        ._productosbloc;
   }
 }
